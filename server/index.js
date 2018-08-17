ae32805580305a1163829006b2ededf018b38f59
Phil Leggetter <phil@leggetter.co.uk>
2018-08-17 16:15:08 +0100

    Server + no-auth JWT endpoint added

---
+++ server/index.js
@@ -0,0 +1,37 @@
+// Load .env config
+require('dotenv').config({
+    path: __dirname + '/.env'
+});
+
+const Nexmo = require('nexmo');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const userAcl = {
+    "paths": {
+        "/v1/users/**": {},
+        "/v1/conversations/**": {},
+        "/v1/sessions/**": {},
+        "/v1/knocking/**": {}
+      }
+}
+
+// endpoint that doesn't authenticate the user
+// it will simply return a JWT with every request
+app.get('/no-auth', (req, res) => {
+    const jwt = Nexmo.generateJwt(process.env.NEXMO_PRIVATE_KEY, {
+        application_id: process.env.NEXMO_APP_ID,
+        sub: process.env.NEXMO_APP_USER,
+        exp: new Date().getTime() + 86400,
+        acl: userAcl
+    })
+
+    res.json({userJwt: jwt})
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
