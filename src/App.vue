7405c8affd4f6e4e97d909d2533192d38f2aad42
Phil Leggetter <phil@leggetter.co.uk>
2018-08-17 12:07:48 +0100

    Adding UI Basics

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CallFromBrowser/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CallFromBrowser from './components/CallFromBrowser.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CallFromBrowser
   }
 }
 </script>
--- src/components/CallFromBrowser.vue
+++ src/components/CallFromBrowser.vue
@@ -1,58 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main class="call-from-browser">
+    <vue-tel-input @onInput="onInput">
+    </vue-tel-input>
+    <button class="call-control" v-bind:class="{'call-in-progress': callInProgress}"
+            v-on:click="controlCallClick"></button>
+    
+    <p>{{infoMessage}}</p>
+  </main>
 </template>
 
 <script>
+import 'vue-tel-input/dist/vue-tel-input.css'
+import VueTelInput from 'vue-tel-input'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'CallFromBrowser',
+  components: {
+      'vue-tel-input': VueTelInput
+  },
+  
+  data() {
+    return {
+      phone: {
+        number: '',
+        isValid: false,
+        country: {}
+      },
+      infoMessage: "",
+      callStatus: "none"
+    }
+  },
+
+  computed: {
+    callInProgress() {
+      return false
+    }
+  },
+
+  methods: {
+
+    onInput({ number, isValid, country }) {
+      this.phone.number = number;
+      this.phone.isValid = isValid;
+      this.phone.country = country;
+
+      if(!isValid) {
+        this.infoMessage = "Please enter a valid phone number"
+      }
+      else {
+        this.infoMessage = `Thanks for entering a valid ${this.phone.country.name} phone number`
+      }
+    },
+
+    controlCallClick() {
+    }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.vue-tel-input {
+  width: 200px;
+  margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.call-control {
+  font-size: 11em; 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.call-control:before {
+  content: '☎️';
 }
-a {
-  color: #42b983;
+
+.call-control.off-hook:before {
+  content: '📞'
 }
 </style>
